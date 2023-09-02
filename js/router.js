c21d26700125866abd9ade3b8316c3f550654429
Flavia Miranda <86119112+FlaMi74@users.noreply.github.com>
2023-09-02 20:56:58 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -18,7 +18,7 @@ route(event) {
 handle() {
     const { pathname } = window.location
     const route = this.routes[pathname] || this.routes[404]
-
+    console.log(this.routes[pathname])
     fetch(route)
     .then(data => data.text())
  
