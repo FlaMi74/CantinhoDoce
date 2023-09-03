0a8b463f61b0a39b4207371938f2a6fc4c1fca8c
Flavia Miranda <86119112+FlaMi74@users.noreply.github.com>
2023-09-03 17:41:49 -0300

    Update index.js
    /

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ window.route = () => router.route()
 
 const router = new Router()
 
-router.add("/home" ,"CantinhoDoce/pages/home.html")
+router.add("/" ,"CantinhoDoce/pages/home.html")
 router.add("/quemsou", "CantinhoDoce/pages/whoIAm.html")
 router.add("/suspiros", "CantinhoDoce/pages/candyType.html")
 router.add("/lembrancinhas", "CantinhoDoce/pages/souveniers.html")
