573ed45bdc2d60da9800cfea720f02e9b4bc71fa
Flavia Miranda <86119112+FlaMi74@users.noreply.github.com>
2023-09-02 21:07:45 -0300

    Update router.js
    .event

--- js/router.js
+++ js/router.js
@@ -7,7 +7,7 @@ add(routeName, page) {
 }
 
 route(event) {
-    event = event || window
+    event = event || window.event
     event.preventDefault()
   
     window.history.pushState({}, "", event.target.href)
@@ -18,7 +18,7 @@ route(event) {
 handle() {
     const { pathname } = window.location
     const route = this.routes[pathname] || this.routes[404]
-    console.log(this.routes[pathname])
+    console.log(route)
     fetch(route)
     .then(data => data.text())
  
