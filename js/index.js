665ff21c2b60c8336b99c2ced421ac9cb2f3e41a
Flavia Miranda <86119112+FlaMi74@users.noreply.github.com>
2023-09-02 21:46:11 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@ import {Router} from "./router.js"
 
 const router = new Router()
 
-router.add("/" ,"../pages/home.html")
-router.add("/quemsou", "../pages/whoIAm.html")
-router.add("/suspiros", "../pages/candyType.html")
-router.add("/lembrancinhas", "../pages/souveniers.html")
-router.add("/contato", "../pages/contact.html")
-router.add(404, "../pages/404.html")
+router.add("/" ,"CantinhoDoce/pages/home.html")
+router.add("/quemsou", "CantinhoDoce/pages/whoIAm.html")
+router.add("/suspiros", "CantinhoDoce/pages/candyType.html")
+router.add("/lembrancinhas", "CantinhoDoce/pages/souveniers.html")
+router.add("/contato", "CantinhoDoce/pages/contact.html")
+router.add(404, "CantinhoDoce/pages/404.html")
 
+console.log(router)
   router.handle()
 
   window.onpopstate = () => router.handle()
