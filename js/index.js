7069463e479cfc68050c2666bcfdcab3dcc9f1ca
Flavia Miranda <86119112+FlaMi74@users.noreply.github.com>
2023-09-02 21:03:28 -0300

    Update index.js
    ./
    ./js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-import {Router} from "./router.js"
+import {Router} from "./js/router.js"
 
 const router = new Router()
 
-router.add("/" ,"/pages/home.html")
-router.add("/quemsou", "/pages/whoIAm.html")
-router.add("/suspiros", "/pages/candyType.html")
-router.add("/lembrancinhas", "/pages/souveniers.html")
-router.add("/contato", "/pages/contact.html")
-router.add(404, "/pages/404.html")
+router.add("/" ,"./pages/home.html")
+router.add("/quemsou", "./pages/whoIAm.html")
+router.add("/suspiros", "./pages/candyType.html")
+router.add("/lembrancinhas", "./pages/souveniers.html")
+router.add("/contato", "./pages/contact.html")
+router.add(404, "./pages/404.html")
 
   router.handle()
 
