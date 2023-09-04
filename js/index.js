0af9d2430a349bf898fbc25ff50c6d2bac05d3e8
Flavia Miranda <86119112+FlaMi74@users.noreply.github.com>
2023-09-03 22:20:40 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import {Router} from "./router.js"
 
 const router = new Router()
 
-router.add("/CantinhoDoce/", "pages/home.html")
+router.add("/CantinhoDoce/", "pages/home2.html")
 router.add("/home" , "CantinhoDoce/pages/home.html")
 router.add("/quemsou", "CantinhoDoce/pages/whoIAm.html")
 router.add("/suspiros", "CantinhoDoce/pages/candyType.html")
